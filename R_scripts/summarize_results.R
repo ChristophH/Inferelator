8eceb03c336a6bb60d0619096d8d4cd24045e326
Christoph Hafemeister <christoph.hafemeister@nyu.edu>
2015-11-03 12:25:44 -0500

    cleaned up sum.net in summarize_results.R

--- R_scripts/summarize_results.R
+++ R_scripts/summarize_results.R
@@ -16,12 +16,7 @@ to.confidence.score <- function(mat) {
   return(1 - (tmp - 1) / (max(tmp) - 1))
 }
 
-sum.net <- function(betas, betas.resc, comb.confs, IN, cc.file) {
-  
-  if (is.null(colnames(comb.confs))) {
-    colnames(comb.confs) <- IN$tf.names
-    rownames(comb.confs) <- rownames(IN$exp.mat)
-  }
+sum.net <- function(betas, betas.resc, comb.confs, IN, cc.file, th=0.5) {
 
   beta.sign <- as.matrix(betas[[1]] * 0)
   beta.non.zero <- as.matrix(betas[[1]] * 0)
@@ -32,9 +27,9 @@ sum.net <- function(betas, betas.resc, comb.confs, IN, cc.file) {
   dimnames(beta.sign) <- dimnames(comb.confs)
   dimnames(beta.non.zero) <- dimnames(comb.confs)
   
-  # we only care about interactions that are present in at least one bootstrap
-  non.zero <- which(beta.non.zero != 0, arr.ind=TRUE)
-  vec.ind <- which(beta.non.zero != 0)
+  # we only care about interactions that are present in more than th (fraction) bootstraps
+  non.zero <- which(beta.non.zero > (length(betas) * th), arr.ind=TRUE)
+  vec.ind <- which(beta.non.zero > (length(betas) * th))
   
   # get mean beta score
   betas.bs <- lapply(betas, function(x) x[vec.ind])
@@ -112,76 +107,10 @@ sum.net.cc <- function(cc.file) {
   load(cc.file)
   load(gsub('combinedconf', 'betas', cc.file))
   load(paste(in.dir, 'params_and_input.RData', sep='/'))
-  
-  tmp <- net.stab(betas.resc, N = 2290)
-  dev.new()
-  plot(tmp[, 1], tmp[, 2])
-  browser()
+
   sum.net(betas, betas.resc, comb.confs, IN, cc.file)
 }
 
-# network stability over the bootstraps
-net.stab <- function(betas.resc, N=5000) {
-  #el <- edge.list(betas.resc, N)
-  el <- edge.list.var(betas.resc, 0.2)
-  nbs <- length(betas.resc)
-  
-  x <- 2:nbs
-  #overlap <- sapply(x, function(i) length(intersect(el[[nbs+i]], el[[nbs+i-1]])))
-  overlap <- sapply(x, function(i) length(intersect(el[[nbs+i]], el[[nbs+i-1]])) / length(el[[nbs+i-1]]))
-  return(cbind(x, overlap))
-  
-  #x <- 1:nbs
-  #overlap <- sapply(x, function(i) length(intersect(el[[nbs+i]], el[[nbs+nbs]])))
-  #return(cbind(x, overlap))
-}
-
-edge.list <- function(betas.resc, N) {
-  nbs <- length(betas.resc)
-  cat('edge.list;', nbs, 'bootstraps; N =', N, '\n')
-  indiv.edges <- list()
-  cum.edges <- list()
-  cum.ranking <- matrix(0, nrow(betas.resc[[1]]), ncol(betas.resc[[1]]))
-  cum.conf.score <- matrix(0, nrow(betas.resc[[1]]), ncol(betas.resc[[1]]))
-  for (bs in 1:length(betas.resc)) {
-    cat('.')
-    bs.mat <- as.matrix(betas.resc[[bs]])
-    #cum.ranking <- cum.ranking + rank(-bs.mat)
-    #cum.edges[[bs]] <- order(cum.ranking)[1:N]
-    cum.conf.score <- cum.conf.score + to.confidence.score(-bs.mat)
-    cum.edges[[bs]] <- order(cum.conf.score, decreasing=TRUE)[1:N]
-    indiv.edges[[bs]] <- order(bs.mat, decreasing=TRUE)[1:N]
-  }
-  cat('done\n')
-  return(c(indiv.edges, cum.edges))
-}
-
-edge.list.var <- function(betas.resc, var.exp.th = 0.2) {
-  nbs <- length(betas.resc)
-  cat('edge.list.var;', nbs, 'bootstraps; var.exp.th =', var.exp.th, '\n')
-
-  beta.non.zero <- as.matrix(betas.resc[[1]] * 0)
-  for (n in 1:nbs) {
-    beta.non.zero <- beta.non.zero + (betas.resc[[n]] != 0)
-  }
-  
-  # we only care about interactions that are present in at least one bootstrap
-  vec.ind <- which(beta.non.zero != 0)
-
-  resc.betas <- lapply(betas.resc, function(x) x[vec.ind])
-  resc.betas <- matrix(unlist(resc.betas), length(resc.betas[[1]]))
-
-  indiv.edges <- list()
-  cum.edges <- list()
-  for (bs in 1:nbs) {
-    cat('.')
-    indiv.edges[[bs]] <- which(resc.betas[, bs] > var.exp.th)
-    cum.edges[[bs]] <- which(apply(resc.betas[, 1:bs, drop=FALSE], 1, median) > var.exp.th)
-  }
-
-  cat('done\n')
-  return(c(indiv.edges, cum.edges))
-}
 
 #v13.medium <- '/home/ch1421/Projects/Rice/inferelator_output/ALL_1505_upstr_or_0_9_50_adjpth001_incl_autoreg_filtered_16_61224_518_3761_tfa_Peking_plus_lit_BBSR_1_20/combinedconf_frac_tp_100_perm_1--frac_fp_0_perm_1_1.RData'
 #sum.net.cc(v13.medium)
@@ -198,7 +127,8 @@ edge.list.var <- function(betas.resc, var.exp.th = 0.2) {
 #v23.small.pr <- '/home/ch1421/Projects/Rice/inferelator_output/150622_allmotifs_694bp_open_expr_adjpth001_incl_autoreg_merged005_filtered_32_47842_462_3364_462tfs_BBSR_1.1_20/combinedconf_frac_tp_100_perm_1--frac_fp_0_perm_1_1.1.RData'
 #v24.small.pr <- '/home/ch1421/Projects/Rice/inferelator_output/150622_allmotifs_694bp_open_expr_adjpth001_incl_autoreg_merged005_filtered_32_47842_462_3364__462tfs_sspr_BBSR_1.1_20/combinedconf_frac_tp_100_perm_1--frac_fp_0_perm_1_1.1.RData'
 #v24.small.pr.41bs <- '/home/ch1421/Projects/Rice/inferelator_output/150622_allmotifs_694bp_open_expr_adjpth001_incl_autoreg_merged005_filtered_32_47842_462_3364_lesstfs_sspr_BBSR_1.1_41/combinedconf_frac_tp_100_perm_1--frac_fp_0_perm_1_1.1.RData'
+#cc.v31 <- '/home/ch1421/Projects/Rice/inferelator_output/151013_590bp_open_expr_adjpth001_exprtfs_incl_autoreg_merged005_filtered_32_sspr_BBSR_1.1_201/combinedconf_frac_tp_100_perm_1--frac_fp_0_perm_1_1.1.RData'
 
 #sum.net.cc(v23.small.pr)
-#sum.net.cc(v24.small.pr.41bs)
+#sum.net.cc(cc.v31)
 
